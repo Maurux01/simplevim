53a2ed8ee25a1a5249ffd6e22eaeb81a267327cc
Mauro Infante <maurux01@gmail.com>
2025-08-29 12:25:51 -0500

    ds

--- lua/plugins/cmdline.lua
+++ lua/plugins/cmdline.lua
@@ -1,3 +1,17 @@
 return {
-  -- Command line suggestions disabled to prevent split window issues
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-cmdline",
+    },
+    opts = function(_, opts)
+      -- Command line completion
+      require("cmp").setup.cmdline(":", {
+        mapping = require("cmp").mapping.preset.cmdline(),
+        sources = require("cmp").config.sources({
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
 }
