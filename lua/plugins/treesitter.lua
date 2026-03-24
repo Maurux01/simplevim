ecef2089684e6a6941bd70160dc6eac35ce01358
Mauro Infante <maurux01@gmail.com>
2026-03-23 22:19:29 -0500

    rainbow bracket function on

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -36,6 +36,11 @@ return {
   opts = {
     highlight = { enable = true },
     indent = { enable = true },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+      max_file_lines = 1000,
+    },
     ensure_installed = {
       "bash",
       "c",
